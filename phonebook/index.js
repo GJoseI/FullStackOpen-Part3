e304c86062497336c2c928fed9ef5eeb214bfd2b
GJoseI <joseignaciogauna2@gmail.com>
2026-04-17 18:46:27 -0300

    3.9

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan")
+const cors = require('cors')
 const app = express();
 
-morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
+app.use(cors())
 app.use(express.json());
+morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+//app.use(express.static('dist'))
 
 let persons = [
   {
@@ -90,7 +93,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
