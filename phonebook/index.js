01391c81d3f4ae31df9ee30ae89a014b0f6a044a
GJoseI <joseignaciogauna2@gmail.com>
2026-04-17 19:22:56 -0300

    3.10 $ 3.11

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const morgan = require("morgan")
-const cors = require('cors')
 const app = express();
 
-app.use(cors())
 app.use(express.json());
 morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-//app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 let persons = [
   {
