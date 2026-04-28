5af2cece36bf5a7bf7a6c1427c89c8c3324fca17
GJoseI <joseignaciogauna2@gmail.com>
2026-04-27 21:42:33 -0300

    3.19

--- phonebook/index.js
+++ phonebook/index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
@@ -70,7 +72,7 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
@@ -78,14 +80,8 @@ app.delete("/api/persons/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
@@ -94,7 +90,8 @@ app.post("/api/persons", (request, response) => {
 
   person.save().then((savedPerson) => {
     response.json(savedPerson);
-  });
+  })
+  .catch(error => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
